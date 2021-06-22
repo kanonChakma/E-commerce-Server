123f77f7741ebc68c9ab970fdf286363267353d1
kanon chakma <kanon121chakma@gmail.com>
2021-06-22 13:20:37 +0600

    admin  dashboard

--- middleware/authCheck.js
+++ middleware/authCheck.js
@@ -1,5 +1,7 @@
 const { firestore } = require('firebase-admin');
 const admin=require('../firebase');
+const User=require('../models/user');
+
 exports.authCheck=async (req,res,next)=>{ 
    try{
        const firebaseUser=await admin.auth().verifyIdToken(req.headers.authtoken);
@@ -9,5 +11,17 @@ exports.authCheck=async (req,res,next)=>{
    }catch(err){
      res.status(401).json({err:"invalid or expired token"})
     //  console.log("error are occuring",err);
-   }    
+    }    
 };
+
+exports.adminCheck=async (req,res,next)=>{
+ const {email}=req.user;
+ const adminUser=await User.findOne({email:email}).exec();
+ if(adminUser.role!=='admin'){
+     res.status(403).json({
+       err:"Admin Resource,access denaied"
+     })
+   }else{
+     next();
+   }   
+}
--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,13 @@ const express=require("express");
 const { createOrUpdateuser,currentUser } = require("../controllers/authService");
 
 //import middleware
-const { authCheck } = require("../middleware/authCheck");
+const { authCheck, adminCheck } = require("../middleware/authCheck");
 
 //import router
 const router=express.Router();
 
 router.post("/create-or-update-user",authCheck,createOrUpdateuser);
 router.post("/current-user",authCheck,currentUser)
+router.post("/current-admin",authCheck,adminCheck,currentUser)
+
 module.exports=router;
